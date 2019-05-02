71c517001dbb8b27c410e890e1366ac5a814516a
Anastasija-Jakovleva  <starme@mail.ru>
2019-05-02 20:54:31 +0300

    Create script_03.py and the result - unsplash_image.jpeg

---
+++ script_03.py
@@ -0,0 +1,10 @@
+#script_03.py
+import requests
+
+output_filename = "unsplash_image.jpeg"
+BASE_URL = "https://source.unsplash.com/random"
+
+response = requests.get(BASE_URL)
+print(response.ok)
+with open(output_filename, "wb") as f:
+    f.write(response.content)
