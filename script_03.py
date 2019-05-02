a3fada42b926c02cc6611b30a4f8082d3aecf3ba
Anastasija-Jakovleva  <starme@mail.ru>
2019-05-02 21:09:37 +0300

    Rebuild script_03.py and the results of 4 jpeg files

--- script_03.py
+++ script_03.py
@@ -1,10 +1,11 @@
 #script_03.py
 import requests
 
-output_filename = "unsplash_image.jpeg"
+output_filename = "unsplash"
 BASE_URL = "https://source.unsplash.com/random"
 
-response = requests.get(BASE_URL)
-print(response.ok)
-with open(output_filename, "wb") as f:
-    f.write(response.content)
+for i in range(1,5):
+    response = requests.get(BASE_URL)
+    print(response.ok)
+    with open(output_filename + "_0" + str(i) + ".jpeg", "wb") as f:
+        f.write(response.content)
